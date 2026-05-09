cc022bb26fcd36922fae7cd11e7f8d91c64564e7
AshryverElisa <elisaashryver@hotmail.com>
2026-05-09 21:08:06 +0200

    refactor fluent code for model creating

--- src/AshryverBot.Database/AshryverBotDbContext.cs
+++ src/AshryverBot.Database/AshryverBotDbContext.cs
@@ -12,13 +12,6 @@ public class AshryverBotDbContext(DbContextOptions<AshryverBotDbContext> options
         var token = modelBuilder.Entity<TwitchTokenEntity>();
         token.ToTable("twitch_tokens");
         token.HasKey(x => x.TwitchUserId);
-        token.Property(x => x.TwitchUserId).HasMaxLength(64);
-        token.Property(x => x.Login).HasMaxLength(64).IsRequired();
-        token.Property(x => x.DisplayName).HasMaxLength(128);
-        token.Property(x => x.Email).HasMaxLength(256);
-        token.Property(x => x.AccessToken).IsRequired();
-        token.Property(x => x.RefreshToken).IsRequired();
-        token.Property(x => x.Scopes).HasMaxLength(4096);
         token.HasIndex(x => x.IsBotAccount);
     }
 }
--- src/AshryverBot.Database/Entities/TwitchTokenEntity.cs
+++ src/AshryverBot.Database/Entities/TwitchTokenEntity.cs
@@ -1,15 +1,31 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace AshryverBot.Database.Entities;
 
 public class TwitchTokenEntity
 {
-    public string TwitchUserId { get; set; } = null!;
-    public string Login { get; set; } = null!;
+    [MaxLength(64)]
+    public required string TwitchUserId { get; set; }
+
+    [MaxLength(64)]
+    public required string Login { get; set; }
+
+    [MaxLength(128)]
     public string? DisplayName { get; set; }
+
+    [MaxLength(256)]
     public string? Email { get; set; }
-    public string AccessToken { get; set; } = null!;
-    public string RefreshToken { get; set; } = null!;
+
+    [MaxLength(512)]
+    public required string AccessToken { get; set; }
+
+    [MaxLength(512)]
+    public required string RefreshToken { get; set; }
     public DateTimeOffset ExpiresAt { get; set; }
+
+    [MaxLength(4096)]
     public string Scopes { get; set; } = string.Empty;
+
     public bool IsBotAccount { get; set; }
     public DateTimeOffset CreatedAt { get; set; }
     public DateTimeOffset UpdatedAt { get; set; }
